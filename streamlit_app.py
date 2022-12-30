55bbf1d2cd4864701a41fd070531add7eb773309
Tony Castillo <96387526+TC-bot@users.noreply.github.com>
2022-12-30 10:30:52 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,9 +35,10 @@ try:
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
 
- except URLError as e:
-   streamlit.error()
-#import requests
+except URLError as e:
+  streamlit.error()
+
+  #import requests
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json()) #just writes the data to the screen
 #take the json version of the response and normalize it
